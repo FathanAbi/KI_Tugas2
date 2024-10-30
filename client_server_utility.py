42a6c85bd6b7348d45fcef8b96f641066e7a984a
Fathan Abi <abikarami60@gmail.com>
2024-10-30 09:07:30 +0700

    fix padding issue

--- client_server_utility.py
+++ client_server_utility.py
@@ -1,8 +1,31 @@
 import des
 
+# padding dan unpadding mengguankan PKCS7
+def pad(string, block_size):
+  padding_number = block_size - len(string) % block_size
+  if padding_number == block_size:
+    return string
+  padding = chr(padding_number) * padding_number
+  return string + padding
+
+def unpad(string, block_size):
+  if not string: return string
+  if len(string) % block_size:
+    raise TypeError('string is not a multiple of the block size.')
+  padding_number = ord(string[-1])
+  if padding_number >= block_size:
+    return string
+  else:
+    if all( padding_number == ord(c) for c in string[-padding_number:] ):
+      return string[0:-padding_number]
+    else:
+      return string
+
+
 def handle_to_another_connection(client_socket):
     """Send encrypted data to the other device."""
     text = input("> Masukkan text: ")
+    text = pad(text, 8)
     key = input("> Masukkan secret key (8 karakter): ")
     while len(key) != 8:
         print("> Panjang secret key tidak valid. Silakan masukkan 8 karakter.")
@@ -63,7 +86,7 @@ def handle_from_other_connection(client_socket):
 
     if decryption_mode == 1:
         decrypted_text = des.ecb_process(text, key, mode="decrypt")
-        print(f">> ECB >> Teks Terdekripsi: {des.bin_to_text(decrypted_text)}")
+        print(f">> ECB >> Teks Terdekripsi: {unpad(des.bin_to_text(decrypted_text), 8)}")
     elif decryption_mode == 2:
         iv = input(">> Masukkan initial vector (8 karakter): ")
         while len(iv) != 8:
@@ -71,7 +94,7 @@ def handle_from_other_connection(client_socket):
             iv = input(">> Masukkan initial vector (8 karakter): ")
 
         decrypted_text = des.cbc_process(text, key, iv, mode="decrypt")
-        print(f">> CBC >> Teks Terdekripsi: {des.bin_to_text(decrypted_text)}")
+        print(f">> CBC >> Teks Terdekripsi: {unpad(des.bin_to_text(decrypted_text), 8)}")
     elif decryption_mode == 3:
         iv = input(">> Masukkan initial vector (8 karakter): ")
         while len(iv) != 8:
@@ -79,6 +102,6 @@ def handle_from_other_connection(client_socket):
             iv = input(">> Masukkan initial vector (8 karakter): ")
 
         decrypted_text = des.cfb_process(text, key, iv, mode="decrypt")
-        print(f">> CFB >> Teks Terdekripsi: {des.bin_to_text(decrypted_text)}")
+        print(f">> CFB >> Teks Terdekripsi: {unpad(des.bin_to_text(decrypted_text), 8)}")
     else:
         print("> Opsi tidak valid, silakan coba lagi.")
